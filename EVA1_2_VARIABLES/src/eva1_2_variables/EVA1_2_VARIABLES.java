d9d75e5526f4c35d3829519075efa24faa46b7e7
24550261 <L24550261@chihuahua2.tecnm.mx>
2024-08-27 16:04:25 -0600

    Add files via upload

---
+++ EVA1_2_VARIABLES/src/eva1_2_variables/EVA1_2_VARIABLES.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_variables;
+
+/**
+ *
+ * @author Ivan
+ */
+public class EVA1_2_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Las notas son utiles para explicar el codigo para nosotros los programadores
+       
+        short edad; //Declaro una variable entera de tipo edad
+        double age; //No se puede declarar 2 veces una misna variable 
+        String nombre; // en amarillo es una dvertencia por uso innecesario de espacio
+         String Apellido;
+         double prom;
+         edad=18; //estoy asignando el valor de 18 dentro de la variale edad
+         
+        System.out.println("Hola Mundo!!"); //Se pueden imrpimir datos de tipo texto
+        System.out.println(100); //se pueden imprimir datos de tipo numerico
+        System.out.println('a'); //se pueden imprimir datos de tipo caracter
+        System.out.println(false);//valore logicos 
+        System.out.println(true);
+        System.out.println(1000.43); //Numeros con fraccion (con punto flotante)
+        System.out.println(edad);//Aqui le estoy diciendo a println que me imprima el valor de la variable que le indique 
+        
+        /*
+        tambien se pueden hacer notas mas largas que abarquen
+        mas lineas 
+        */
+        //Creacion de variables 
+        //Declaracion
+        /*Tipo de dato /nombre de la varible
+            que operaciones debe o puede hacer 
+        cuanta memoria va a utilizar
+        
+        
+        */
+        //Asignacion
+    }
+    
+}

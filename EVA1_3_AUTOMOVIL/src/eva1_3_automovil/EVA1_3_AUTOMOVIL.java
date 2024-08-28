a579c37bb57ed8145af2ccb1846661527311d526
24550261 <L24550261@chihuahua2.tecnm.mx>
2024-08-28 16:27:47 -0600

    Add files via upload

---
+++ EVA1_3_AUTOMOVIL/src/eva1_3_automovil/EVA1_3_AUTOMOVIL.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_automovil;
+
+/**
+ *
+ * @author Ivan
+ */
+public class EVA1_3_AUTOMOVIL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Declaracion de las variables 
+        String marca;
+        String modelo;
+        double precio;
+        int year;
+        //Asignar valores 
+        marca="Ford";
+        modelo="Mustang";
+        year=2022;
+        precio=999000.1;
+        
+        System.out.println(marca); //imprime el contenido de la vairable marca 
+        System.out.println(modelo); //Imprime el contenido de la variable modelo
+        System.out.println(year); //Imprime el contenido de la variable year
+        System.out.println(precio); //Imprime el contenido de la variable precio
+        
+        
+    }
+    
+}

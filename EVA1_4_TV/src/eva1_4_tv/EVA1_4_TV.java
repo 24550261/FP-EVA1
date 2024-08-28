d188622950d7af1162a9131c4a28aa286c22a986
24550261 <L24550261@chihuahua2.tecnm.mx>
2024-08-28 16:28:45 -0600

    Add files via upload

---
+++ EVA1_4_TV/src/eva1_4_tv/EVA1_4_TV.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_tv;
+
+/**
+ *
+ * @author Ivan
+ */
+public class EVA1_4_TV {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String marca;
+        int pulgadas;
+        double precio;
+        String Calidad;
+        String modelo;
+        String tecno;
+        
+        marca="Samsung";
+        pulgadas= 43;
+        precio=6599.99;
+        Calidad="Full HD";
+        modelo="T5300 Smart TV";
+        tecno="QLED";
+
+
+            System.out.println(marca);
+            System.out.println(modelo);
+            System.out.println(pulgadas);
+            System.out.println(Calidad);
+            System.out.println(precio);
+            System.out.println(tecno);
+        
+        
+                
+        
+    }
+    
+}
